383c0581f33b54aedf27762cbeeb006384978b98
Earl Hans Genoso <earlhansg@gmail.com>
2020-03-09 02:52:46 +0800

    #9 Implementing AuthGuards for lazy routes

--- src/app/music/music-routing.module.ts
+++ src/app/music/music-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { AuthGuard } from '@app/auth/shared/guards/auth.guard';
+
 
 export const ROUTES: Routes = [
   {
     path: 'songs',
+    canActivate: [AuthGuard],
     loadChildren: './songs/songs.module#SongsModule'
   }
 ];
