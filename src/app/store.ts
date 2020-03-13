0ce0f0903afe91e12a1135610f817567ac8e2b76
Earl Hans Genoso <earlhansg@gmail.com>
2020-03-13 23:36:51 +0800

    #10 added songs in the State

--- src/app/store.ts
+++ src/app/store.ts
@@ -4,15 +4,18 @@ import { Observable, BehaviorSubject } from 'rxjs';
 import { pluck, distinctUntilChanged } from 'rxjs/operators';
 
 import { User } from './auth/shared/services/auth.service';
+import { Song } from './music/shared/services/songs.service';
 
 
 export interface State {
   user: User;
+  songs: Song[];
   [key: string]: any;
 }
 
 const state: State = {
-  user: undefined
+  user: undefined,
+  songs: undefined
 };
 
 @Injectable({ providedIn: 'root' })
