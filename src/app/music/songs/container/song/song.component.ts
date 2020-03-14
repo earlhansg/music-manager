6f428ce64c861656a62020eac196e876ca689389
Earl Hans Genoso <earlhansg@gmail.com>
2020-03-14 22:30:29 +0800

    #11 Setup Song component for CRUD (initial setup)

--- src/app/music/songs/container/song/song.component.ts
+++ src/app/music/songs/container/song/song.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+
+import { Observable, Subscription } from 'rxjs';
+
+import { Song, SongsService } from '@app/music/shared/services/songs.service';
 
 @Component({
   selector: 'app-song',
@@ -7,8 +12,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SongComponent implements OnInit {
 
-  constructor() {}
+  song$: Observable<Song>;
+  subscription: Subscription;
 
-  ngOnInit() {}
+  constructor(
+    private songsService: SongsService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    // this.subscription = this.songsService.songs$.subscribe();
+  }
+
+
+  async addSong(event: Song) {
+    this.backToSongs();
+  }
+
+  async updateSong(event: Song) {
+  }
+
+  async removeSong(event: Song) {
+
+  }
+
+  backToSongs() {
+    this.router.navigate(['songs']);
+  }
 
 }
